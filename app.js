bb741ae0ae2fa0cf54a6735283ac354192a91f15
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2023-01-20 23:45:39 +0600

    date added

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ const days = document.querySelector(".days");
 const hours = document.querySelector(".hours");
 const minutes = document.querySelector(".minutes");
 const seconds = document.querySelector(".seconds");
+
+const year = new Date().getFullYear();
+const todaysDate = new Date(`${year}-1-20`);
+const openingDate = new Date(`${year}-5-20`);
